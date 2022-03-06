c8f5fb61d505a6d7aa8d5eeb26e86bd4ccd5c548
ernestrudziec <ernest.rudziec@gmail.com>
2022-03-06 19:00:43 +0100

    srednia wazona

--- avg.php
+++ avg.php
@@ -7,3 +7,14 @@ function srednia()
 
 echo srednia(1, 2, 5, 3, 4, 2, 2, 1, 3);
 ?>
+<?php
+$oceny = array(1,2,3,4,5);
+$wagi = array(5,4,3,2,1);
+$sumaWazona = 0;
+foreach( $oceny as $klucz => $ocena ){
+  $sumaWazona +=  $ocena * $wagi[$klucz];
+  $sumaWag += $wagi[$klucz];
+}
+ 
+$wynik = $sumaWazona / $sumaWag;
+?>
